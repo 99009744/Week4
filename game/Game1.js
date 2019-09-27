f957e624b06b421eef7e5aab5e6faa45645dc068
99009744 <99009744@mydavinci.nl>
2019-09-27 13:42:58 +0200

    klaar met de vragen
    klaar met de vragen

--- game/Game1.js
+++ game/Game1.js
@@ -18,11 +18,54 @@
 						alert ("Goed bezig! 8 gigabite is het minimale voor een goede game pc!")
 						answer = prompt("Wil je een voeding in je pc? \n kies ja of nee")
 						if (answer == "ja"){
-							alert ("Ja deze heb je zeker nodig hoe sluit je anders je pc aan op het stroom!")
-							answer = prompt ("Wil je een GPU in je pc? \n kies ja of nee")
-							if (answer == "ja")
-								alert ("Deze is wel erg belangrijk in je game pc anders draaien je games niet zo fijn!")
+							alert ("Deze heb je zeker nodig hoe ga je anders je pc op het stroom aansluiten!")
+							answer = prompt("Wil je een gpu in je pc? \n ja of nee")
+							if (answer == "ja"){
+								alert ("Deze gaat wel nodig zijn als je wilt gamen!")
+								answer = prompt("Wil je koeling voor je cpu in je pc? \n ja of nee")
+								if (answer == "ja"){
+									alert ("Deze heb je zeker nodig want die zorgt er voor dat je cpu niet over verhit raakt!")
+									answer = prompt("Wil je een behuizing voor je pc?\n ja of nee")
+									if (answer =="ja"){
+										alert ("Ja deze heb je nodig hier kun je natuurlijk al je componenten in bouwen!")
+										alert ("Zo! je pc werkt nu, alleen nog even externe componenten er bij nemen")
+										answer = prompt("Wil je er een toetsenbord bij?\n ja of nee")
+										if (answer=="ja"){
+											alert ("Deze heb je zeker nodig ander kun je niet typen.")
+											answer = prompt("Wil je er een muis bij? \n ja of nee")
+											if (answer=="ja"){
+												alert ("Dit maakt het gebruiken van je pc wel een stuk makkelijk ja!")
+												answer = prompt ("Wil je er een monitor bij?\n ja of nee")
+												if (answer=="ja"){
+													alert ("Natuurlijk ander kun je je beeld niet zien!")
+													alert ("Zo we zijn klaar! je pc werkt naar behoren, goed gedaan!")
 
+												}
+												else{
+													alert("Zonder monitor kun je niet zien wat er gebeurd op je pc!")
+												}
+
+											}
+											else{
+												alert("Zonder word het erg moeilijk om je pc te besturen.")
+											}
+										}
+										else{
+											alert ("Hoe wil je gaan typen zonder toetsenbord! Dat word wel erg lastig.")
+										}
+									}
+									else{
+										alert ("Waar ga je al je componenten in stoppen?")
+									}
+								}
+								else{
+									alert ("Zonder gaat je pc oververhit raken en dat wil je niet!")
+								}
+
+							}
+							else{
+								alert ("Zonder gpu gaan je games niet fijn lopen, deze is wel hard nodig!")
+							}
 						}
 						else {
 							alert ("Hoe ga je je pc anders aansluiten op het stroom!")
